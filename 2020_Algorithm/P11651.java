f98cb13fab65ef6003041a37c758df75d5e5e520
ire4564 <ire4564@gmail.com>
2020-07-09 22:25:05 +0900

    2020-07-09 알고리즘 문제 정렬 문제 (어제와 비슷한 문제)

---
+++ 2020_Algorithm/P11651.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class P11651 {
+	public static void main (String args[]) throws NumberFormatException, IOException {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		int arr[][] = new int[num][2];
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				arr[i][j] = scan.nextInt();
+			}
+		}
+		//ÀçÁ¤ÀÇ
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				
+				if(o1[1] == o2[1]) {
+					return Integer.compare(o1[0], o2[0]);
+				}
+				return Integer.compare(o1[1], o2[1]);
+			}
+			
+		});
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+	}
+}
